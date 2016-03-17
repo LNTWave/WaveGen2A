b148bea9eb9a8c138a8f7c639fd96bf9ae566fcf
john O <jowen@nextivityinc.com>
2016-03-17 12:55:32 -0700

    a

--- js/msg_azure.js
+++ js/msg_azure.js
@@ -26,7 +26,7 @@ function SendAzureData( )
 
     var nType = "POST";
 //    var nUrl  = "https://myIotHubYavuz.azure-devices.net/devices/myFirstDevice/messages/events?api-version=2015-08-15-preview"
-    var nUrl  = "https://myIotHubYavuz.azure-devices.net/devices/myFirstDevice/messages/events?api-version=2015-08-15-preview"
+    var nUrl  = "https://NextivityIoTHubDev.azure-devices.net/devices/myFirstDevice/messages/events?api-version=2015-08-15-preview"
     var nContentType = "application/octet-stream";
 //    var nData = "{'deviceId': 'myFirstDevice','App Speed':" + tempCounter + "}";
     var nData = "{'App Speed':" + tempCounter + "}";        // Does not look like we need the deviceId in the data....
